72860dff05dc4255168c5cda1d5a375bfe783b8a
Dieu Nguyen <dieu.nguyen@employmenthero.com>
2018-09-24 21:33:17 +0700

    Add blog bootstrap

---
+++ src/App.js
@@ -0,0 +1,207 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+import './blog.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <header className="blog-header py-3">
+          <div className="row flex-nowrap justify-content-between align-items-center">
+            <div className="col-4 pt-1">
+              <a className="text-muted" href={null}>Subscribe</a>
+            </div>
+            <div className="col-4 text-center">
+              <a className="blog-header-logo text-dark" href={null}>Large</a>
+            </div>
+            <div className="col-4 d-flex justify-content-end align-items-center">
+              <a className="text-muted" href={null}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="mx-3"><circle cx="10.5" cy="10.5" r="7.5"></circle><line x1="21" y1="21" x2="15.8" y2="15.8"></line></svg>
+              </a>
+              <a className="btn btn-sm btn-outline-secondary" href={null}>Sign up</a>
+            </div>
+          </div>
+        </header>
+        <div className="nav-scroller py-1 mb-2">
+          <nav className="nav d-flex justify-content-between">
+            <a className="p-2 text-muted" href={null}>World</a>
+            <a className="p-2 text-muted" href={null}>U.S.</a>
+            <a className="p-2 text-muted" href={null}>Technology</a>
+            <a className="p-2 text-muted" href={null}>Design</a>
+            <a className="p-2 text-muted" href={null}>Culture</a>
+            <a className="p-2 text-muted" href={null}>Business</a>
+            <a className="p-2 text-muted" href={null}>Politics</a>
+            <a className="p-2 text-muted" href={null}>Opinion</a>
+            <a className="p-2 text-muted" href={null}>Science</a>
+            <a className="p-2 text-muted" href={null}>Health</a>
+            <a className="p-2 text-muted" href={null}>Style</a>
+            <a className="p-2 text-muted" href={null}>Travel</a>
+          </nav>
+        </div>
+        <div className="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+          <div className="col-md-6 px-0">
+            <h1 className="display-4 font-italic">Title of a longer featured blog post</h1>
+            <p className="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.</p>
+            <p className="lead mb-0"><a href={null} className="text-white font-weight-bold">Continue reading...</a></p>
+          </div>
+        </div>
+
+        <div className="row mb-2">
+          <div className="col-md-6">
+            <div className="card flex-md-row mb-4 shadow-sm h-md-250">
+              <div className="card-body d-flex flex-column align-items-start">
+                <strong className="d-inline-block mb-2 text-primary">World</strong>
+                <h3 className="mb-0">
+                  <a className="text-dark" href={null}>Featured post</a>
+                </h3>
+                <div className="mb-1 text-muted">Nov 12</div>
+                <p className="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                <a href={null}>Continue reading</a>
+              </div>
+              <img className="card-img-right flex-auto d-none d-lg-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" style={{width: '200px', height: '250px'}} src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20250%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1660bd27a03%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A13pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1660bd27a03%22%3E%3Crect%20width%3D%22200%22%20height%3D%22250%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2256.203125%22%20y%3D%22131%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" />
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="card flex-md-row mb-4 shadow-sm h-md-250">
+              <div className="card-body d-flex flex-column align-items-start">
+                <strong className="d-inline-block mb-2 text-success">Design</strong>
+                <h3 className="mb-0">
+                  <a className="text-dark" href={null}>Post title</a>
+                </h3>
+                <div className="mb-1 text-muted">Nov 11</div>
+                <p className="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                <a href={null}>Continue reading</a>
+              </div>
+              <img className="card-img-right flex-auto d-none d-lg-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20250%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1660bd27a06%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A13pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1660bd27a06%22%3E%3Crect%20width%3D%22200%22%20height%3D%22250%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2256.203125%22%20y%3D%22131%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" style={{width: '200px', height: '250px'}} />
+            </div>
+          </div>
+        </div>
+
+
+        <main role="main" className="container">
+          <div className="row">
+            <div className="col-md-8 blog-main">
+              <h3 className="pb-3 mb-4 font-italic border-bottom">
+                From the Firehose
+              </h3>
+
+              <div className="blog-post">
+                <h2 className="blog-post-title">Sample blog post</h2>
+                <p className="blog-post-meta">January 1, 2014 by <a href={null}>Mark</a></p>
+
+                <p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
+                <hr />
+                <p>Cum sociis natoque penatibus et magnis <a href={null}>dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
+                <blockquote>
+                  <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                </blockquote>
+                <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+                <h2>Heading</h2>
+                <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+                <h3>Sub-heading</h3>
+                <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                <pre><code>Example code block</code></pre>
+                <p>Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
+                <h3>Sub-heading</h3>
+                <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+                <ul>
+                  <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
+                  <li>Donec id elit non mi porta gravida at eget metus.</li>
+                  <li>Nulla vitae elit libero, a pharetra augue.</li>
+                </ul>
+                <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>
+                <ol>
+                  <li>Vestibulum id ligula porta felis euismod semper.</li>
+                  <li>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</li>
+                  <li>Maecenas sed diam eget risus varius blandit sit amet non magna.</li>
+                </ol>
+                <p>Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis.</p>
+              </div>
+              {/* <!-- /.blog-post --> */}
+
+              <div className="blog-post">
+                <h2 className="blog-post-title">Another blog post</h2>
+                <p className="blog-post-meta">December 23, 2013 by <a href={null}>Jacob</a></p>
+
+                <p>Cum sociis natoque penatibus et magnis <a href={null}>dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
+                <blockquote>
+                  <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                </blockquote>
+                <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+                <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+              </div>
+              {/* <!-- /.blog-post --> */}
+
+              <div className="blog-post">
+                <h2 className="blog-post-title">New feature</h2>
+                <p className="blog-post-meta">December 14, 2013 by <a href={null}>Chris</a></p>
+
+                <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+                <ul>
+                  <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
+                  <li>Donec id elit non mi porta gravida at eget metus.</li>
+                  <li>Nulla vitae elit libero, a pharetra augue.</li>
+                </ul>
+                <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+                <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>
+              </div>
+              {/* <!-- /.blog-post --> */}
+
+              <nav className="blog-pagination">
+                <a className="btn btn-outline-primary" href={null}>Older</a>
+                <a className="btn btn-outline-secondary disabled" href={null}>Newer</a>
+              </nav>
+            </div>
+            {/* <!-- /.blog-main --> */}
+
+            <aside className="col-md-4 blog-sidebar">
+              <div className="p-3 mb-3 bg-light rounded">
+                <h4 className="font-italic">About</h4>
+                <p className="mb-0">Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+              </div>
+
+              <div className="p-3">
+                <h4 className="font-italic">Archives</h4>
+                <ol className="list-unstyled mb-0">
+                  <li><a href={null}>March 2014</a></li>
+                  <li><a href={null}>February 2014</a></li>
+                  <li><a href={null}>January 2014</a></li>
+                  <li><a href={null}>December 2013</a></li>
+                  <li><a href={null}>November 2013</a></li>
+                  <li><a href={null}>October 2013</a></li>
+                  <li><a href={null}>September 2013</a></li>
+                  <li><a href={null}>August 2013</a></li>
+                  <li><a href={null}>July 2013</a></li>
+                  <li><a href={null}>June 2013</a></li>
+                  <li><a href={null}>May 2013</a></li>
+                  <li><a href={null}>April 2013</a></li>
+                </ol>
+              </div>
+
+              <div className="p-3">
+                <h4 className="font-italic">Elsewhere</h4>
+                <ol className="list-unstyled">
+                  <li><a href={null}>GitHub</a></li>
+                  <li><a href={null}>Twitter</a></li>
+                  <li><a href={null}>Facebook</a></li>
+                </ol>
+              </div>
+            </aside>
+            {/* <!-- /.blog-sidebar --> */}
+          </div>
+        </main>
+
+
+        <footer className="blog-footer">
+          <p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+          <p>
+            <a href={null}>Back to top</a>
+          </p>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default App;

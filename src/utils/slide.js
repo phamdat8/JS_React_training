bb948dd33eadac66a0455e3435d3051436af6f1a
Dieu Nguyen <dieu.nguyen@employmenthero.com>
2018-10-14 17:53:28 +0700

    Add test for slide.js

--- src/utils/slide.js
+++ src/utils/slide.js
@@ -1,10 +1,15 @@
 // Template for creating a custom Slide object
-function Slide(title, subtitle, background, link, slideIndex ) {
-	this.title = title;
-	this.subtitle = subtitle;
-	this.background = background;
-	this.link = link;
-	this.id = "slide" + slideIndex;
+function Slide(title, subtitle, background, link, slideIndex) {
+  this.title = title;
+  this.subtitle = subtitle;
+  this.background = background;
+  this.link = link;
+  this.id = 'slide' + slideIndex;
+}
+
+export function Add(x, y) {
+  if (x === undefined || y === undefined) return 0;
+  return x + y;
 }
 
 export default Slide;
